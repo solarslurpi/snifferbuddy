8c338dc3735ff07d512184da8281595c41dcabe2
HappyDay <happyday.mjohnson@gmail.com>
2025-01-01 15:17:52 -0800

    evolved.

--- src/logger_setup.py
+++ src/logger_setup.py
@@ -1,9 +1,11 @@
 import logging
 import logging.config
 import yaml
+from importlib.resources import files
 
 def logger_setup(name: str) -> logging.Logger:
     # Load logging configuration from YAML file
+    config_path = files(__name__).joinpath('logging_config.yaml')
     with open('logging_config.yaml', 'r') as f:
         config = yaml.safe_load(f)
 
